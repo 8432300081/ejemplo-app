301b9d26940b6e146e3586b660c874ce940e154c
Guillermo Sosa <gsosa@rednacional.com>
2025-05-17 14:30:00 -0600

    rutas_estaticas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// declaracion de rutas fijas
+Route::get('/Productos',function(){
+    return "Sección de productos";
+});
+
+// deteccion de parametros
+Route::get('Productos/{id}', function ($id) {
+    return "Bienvenido $id";
+});
+
+Route::get('prueba_parametros/{id}', function ($id) {
+    $d = $id;
+    $nombre = explode("-",$d);
+    return "tu nombre es $nombre[1]";
+});
