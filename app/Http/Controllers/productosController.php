2cc1605e42df8993248dd0c2a4f829589a3ad65b
Guillermo Sosa <gsosa@rednacional.com>
2025-06-20 21:04:57 -0600

    Initial commit of Laravel project

---
+++ app/Http/Controllers/productosController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class productoscontroller extends Controller
+{
+    public function index()
+    {
+        return view ("productos/index");
+        }
+ 
+    public function create(){
+        return "Carrito activo";
+    }
+
+    public function show($product){
+        return "el producto es: $product";
+    }
+}
